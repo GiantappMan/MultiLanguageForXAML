a00022699486c239b717cebd7e114a4ec2f781c8
DaZiYuan <mscoder@hotmail.com>
2018-07-31 17:12:28 +0800

    创建项目

---
+++ MultiLanguageManager/Lan.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+#if WINDOWS_UWP
+
+using Windows.UI.Xaml;
+
+#else
+
+using System.Windows;
+
+#endif
+
+
+
+namespace MultiLanguageManager.WPF
+{
+    public class Lan
+    {
+        #region Key
+
+        public static string GetKey(DependencyObject obj)
+        {
+            return (string)obj.GetValue(KeyProperty);
+        }
+
+        public static void SetKey(DependencyObject obj, string value)
+        {
+            obj.SetValue(KeyProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for LKey.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty KeyProperty =
+            DependencyProperty.RegisterAttached("Key", typeof(string), typeof(Lan), new PropertyMetadata(new PropertyChangedCallback(async (sender, e) =>
+            {
+                FrameworkElement element = sender as FrameworkElement;
+                element.Unloaded += Element_Unloaded;
+                var key = e.NewValue.ToString();
+                var lan = await LanService.SharedInstance.Get(key);
+            })));
+
+        private static void Element_Unloaded(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ MultiLanguageManager/LanService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MultiLanguageManager
+{
+    public class LanService
+    {
+        public static LanService SharedInstance { get; private set; } = new LanService();
+
+        public void Init(string jsonDir)
+        {
+
+        }
+
+        public Task<string> Get(string key)
+        {
+            return null;
+        }
+    }
+}
